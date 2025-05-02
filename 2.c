e5c1854ac6ec2172b2de91e9246bb1499ed318c4
olllxa <oklashtorn@gmail.com>
2025-05-02 09:08:37 +0300

    Create 2.c

--- 2.c
+++ 2.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 
 int indexOf(char text[], char word[]) 
 {
-    int wordCount = 0; 
-    int i = 0;
+    int textLength = 0;
+    int targetLength = 0;
 
-    while (text[i] != '\0') {
-        while (text[i] == ' ') {
-            i++;
+    while (text[textLength] != '\0') {
+        textLength++;
+    }
+
+    while (word[targetLength] != '\0') {
+        targetLength++;
+    }
+
+    int foundIndex = -1;
+    for (int i = 0; i <= textLength - targetLength; i++) {
+        int match = 1;
+        for (int j = 0; j < targetLength; j++) {
+            if (text[i + j] != word[j]) {
+                match = 0;
+                break;
+            }
         }
-        if (text[i] == '\0') break;
-
-        wordCount++; 
-
-        int j = 0;
-        int k = i;
-        while (word[j] != '\0' && text[k] != '\0' && text[k] == word[j]) {
-            k++;
-            j++;
-        }
-
-        if (word[j] == '\0' && (text[k] == ' ' || text[k] == '\0')) {
-            return wordCount;  
-        }
-
-        while (text[i] != ' ' && text[i] != '\0') {
-            i++;
+        if (match == 1) {
+            foundIndex = i;
+            break;
         }
     }
 
-    return -1;  
+    return foundIndex;
 }
 
-int main()
+int main ()
 {
     int result = indexOf("the cat sat on the mat", "cat");
     printf("The word is under number: %d\n", result);
